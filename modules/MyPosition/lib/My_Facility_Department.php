cdf47c4feb53a83146eaa780c9578a3f05f4ded0
Sovello Hildebrand <sovellohpmgani@gmail.com>
2019-02-20 17:48:25 -0500

    trying reproduce Aikpe's problem

---
+++ modules/MyPosition/lib/My_Facility_Department.php
@@ -0,0 +1,15 @@
+class My_Facility_Department extends I2CE_List {
+	/**
+	* The main field name used for display a description of a record.
+	*/
+   const MAIN_FIELD = "facility";
+
+   /**
+	* The secondary field name used for displaying a description of a record in combination with the MAIN_FIELD.
+	*/
+   const SEC_FIELD = "department";
+   
+   /**
+	* The sort field name to be used for sorting the display list.  This can't be used with the SEC_FIELD option for display.
+	*/
+}
